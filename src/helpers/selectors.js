9589538d0b605d07c7102d29d22533b57c77eeab
Avivit Weissman <avivit@Omers-MacBook-Pro-old.local>
2021-06-09 17:14:13 -0700

    added getAppointmentsForDay helper fn and tests for it

---
+++ src/helpers/selectors.js
@@ -0,0 +1,11 @@
+import React, { useState  } from "react";
+
+
+export function getAppointmentsForDay(state, day) {
+  const filteredDays = state.days.filter(filterDay => filterDay.name === day);
+  if (!filteredDays[0]) {
+    return [];
+  }
+  const appointments = filteredDays[0].appointments.map(appointment =>  state.appointments[appointment]);
+  return appointments;
+}
